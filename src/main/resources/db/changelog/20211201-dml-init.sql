68ba553a21095f658c1f3452ae1cbf86d328accd
Rojar Smith <rojarsmith@gmail.com>
2021-12-01 21:09:22 +0800

    Again.

--- src/main/resources/db/changelog/20211201-dml-init.sql
+++ src/main/resources/db/changelog/20211201-dml-init.sql
@@ -2,3 +2,5 @@
 --changeset 20211201-dml-init context:dev
 INSERT  INTO  member (username)   VALUES  ('User Name 1');
 
+--changeset 20211201-dml-init-2 context:dev
+INSERT  INTO  member (username)   VALUES  ('User Name 2');
