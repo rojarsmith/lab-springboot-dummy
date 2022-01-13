d2c132f253aebf153ce085c48946a0f35e6bc1b7
Rojar Smith <rojarsmith@gmail.com>
2022-01-13 14:01:44 +0800

    Return value in config

--- src/main/java/lab/springboot/dummy/TestController.java
+++ src/main/java/lab/springboot/dummy/TestController.java
@@ -1,5 +1,6 @@
 package lab.springboot.dummy;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
+	
+	@Value("${properties.name}")
+	private String propertiesName;
+	
 	@GetMapping("hello")
 	@ResponseBody
 	public ResponseEntity<?> sayHello() {
-		return ResponseEntity.ok().body("hello " + Const.Name);
+		return ResponseEntity.ok().body(String.format("hello %s, %s", Const.Name, propertiesName));
 	}
 }
